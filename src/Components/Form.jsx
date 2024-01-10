4be0be054db1276c7898a07047995da152138670
Simon Gustavsson <simgus1234@gmail.com>
2024-01-10 17:23:49 +0100

    Added functional contact form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -10,31 +10,35 @@ const Form = () => {
                     <div className="socials">
                         <h3>My Platforms</h3>
                         <div className="social__platforms">
-                            <a href="www.github.com/yret1"><img src={Github} alt="" /></a>
-                            <a href="www.linkedin.com/simon"><img src={Linked} alt="" /></a>
+                            <a href="https://github.com/yret1" rel="noreferrer" target="_blank"><img src={Github} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/simon-gustavsson-94b50090/" rel="noreferrer" target="_blank"><img src={Linked} alt="" /></a>
                         </div>
                         <h3>Contact Me</h3>
                     </div>
                 </div>
 
-                <form className="form" action="">
+                <form className="form" action="https://formkeep.com/f/32578217fb21"
+                    acceptCharset="UTF-8"
+                    encType="multipart/form-data"
+                    method="POST">
+                         <input type="hidden" name="utf8" value="✓"></input>
 
                     <fieldset>
                         <legend>Name</legend>
                     
-                            <input className="inputbox" type="text" placeholder="Name" />
+                            <input className="inputbox" id="Full_Name" name="Full Name" type="text" placeholder="Name" required/>
                        
                     </fieldset>
 
                     <fieldset>
                         <legend>Email</legend>
-                            <input className="inputbox" type="text" placeholder="Email" />
+                            <input className="inputbox" id="Email-address" name="Email" type="text" placeholder="Email" required/>
                     </fieldset>
 
 
                     <fieldset>
                         <legend>Message</legend>
-                            <textarea className="inputbox" type="text" rows={7} placeholder="Message" />
+                            <textarea className="inputbox" id="Message" name="Message" type="text" rows={7} placeholder="Message" required/>
                     </fieldset>
 
 
